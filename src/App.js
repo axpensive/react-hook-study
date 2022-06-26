524e7c729ae577cf7da647a3c8545611b50468db
axpensive <35427211+axpensive@users.noreply.github.com>
2022-06-26 19:46:03 +0900

    multiple state

--- src/App.js
+++ src/App.js
@@ -1,48 +1,27 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
-const App = () => {
-  //分割代入
-  const [count, setCount] = useState(0)
+const App = props => {
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
 
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
-
-  const increment2 = () => setCount(previousCount => previousCount + 1)
-  const decrement2 = () => setCount(previousCount => previousCount - 1)
-  
-  const reset = () => setCount(0)
-  const double = () => setCount(previousCount => previousCount * 2)
-  const divide3 = () => setCount(previousCount => {
-    if (previousCount % 3 !== 0){
-      return previousCount
-    }
-    return previousCount /3 
-  })
-
-  //jsxの仕様で一つのタグしか返せない。複数返したい場合はタグでラッピングする。
-  // <></>はReact.Fragmentと同じ
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
   return (
     <>
-    <div>
-      count:{count}
-      </div> 
-      <div>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={double}>x2</button>
-        <button onClick={divide3}>3の倍数のときだけ3で割る</button>
-      </div>
-
+      <p>現在の{name}は{price}円です。</p>
+      <button onClick={() => setPrice(price + 1)}> price +1</button>
+      <button onClick={() => setPrice(price - 1)}> price -1</button>
+      <input value={name} onChange={e => setName(e.target.value)} />
+      <button onClick={() => reset()}>Reset</button>
     </>
-    
-  );
+  )
 }
 
-export default App;
+App.defaultProps = {
+  name: '初期値',
+  price: 1000
+}
+
+export default App
